84412c5c9d288a6d2cc41a1a2f73cc6695e13cc1
unknown <talktoitopa@gmail.com>
2022-08-10 09:58:06 +0100

    dotnet app

---
+++ FactorialApp/FactorialApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class FactorialApp2
+{
+    static void Main(string[] args)
+    // Declare variables and then initialize to zero.
+    {
+        int num;
+
+        // Display title as the C# console calculator app.
+        Console.WriteLine("Console Factorial Calculator in C#\r");
+        Console.WriteLine("------------------------\n");
+
+        // Ask the user to type the first number.
+        Console.WriteLine("Type a number, and then press Enter");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i <= num; i++)
+            if (i == num)
+                Console.WriteLine($"Your result:" + 1);
+            else
+                Console.WriteLine($"Your result: " + (i * (i - 1)));
+
+        // Wait for the user to respond before closing.
+        Console.Write("Press any key to close the Factorial Calculator console app...");
+        Console.ReadKey();
+    }
+}
